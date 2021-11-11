f744a164ca1a7e5be6515b033964992bcaa86b0f
marcellinus-witarsah <aw.marcel@gmail.com>
2021-11-11 07:39:52 +0700

    Update dari marcel perubahan header dan foote runtuk sign up tutor

--- src/pages/tutor/signup_tutor_4.php
+++ src/pages/tutor/signup_tutor_4.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../../css/login.css">
-    
+
     <!-- <link rel="stylesheet" href="../../css/signup_tutor.css"> -->
 </head>
 
@@ -38,32 +38,8 @@
         </div>
     </div>
 
-    <footer class="flex-container-column flex-center footer">
-        <div class="flex-container-row flex-between footer-menu">
-            <ul class="footer-menu__left">
-                <li><b>Tentang</b></li>
-                <li>Siapa kami? </li>
-                <li>Syarat dan Ketentuan</li>
-                <li>Kebijakan Privasi</li>
-                <li>FAQ</li>
-            </ul>
-            <ul class="footer-menu__center">
-                <li><b>Kelas</b></li>
-                <li>Mulai Kelas</li>
-                <li>Lihat subjek kami</li>
-            </ul>
-            <ul class="footer-menu__right">
-                <li><b>Menjadi tutor</b></li>
-                <li>Keuntungan menjadi tutor</li>
-                <li>Daftar menjadi tutor</li>
-                <li>Masuk</li>
-            </ul>
-        </div>
-        <div class="width-1280">
-            <hr class="line">
-            <p align="center">© Pelitaku, 2021</p>
-        </div>
-    </footer>
+    <?php include '../../assets/php/footer.php' ?>
+
 </body>
 
 </html>
