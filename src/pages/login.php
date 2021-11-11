b88af26f3d4e75be0293fb61c60444a169a0ef2d
marcellinus-witarsah <aw.marcel@gmail.com>
2021-11-11 12:08:28 +0700

    update login.php

--- src/pages/login.php
+++ src/pages/login.php
@@ -1,49 +1,49 @@
 <?php
-    // session_start(); 
-    // if(isset($_SESSION['id'])){
-    //     echo "<script type='text/javascript'>window.history.go(-1)</script>";
-    // }
-    $passwordErr = $emailErr = $password = $email = $dbErr = "";
-    error_reporting(E_ERROR | E_PARSE);
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        echo "<script type='text/javascript'> showLogin()</script>";
-        if (empty($_POST["email"])) {
-            $emailErr = "Email is required";
-        } else {
-            $email = test_input($_POST["email"]);
-            // check if e-mail address is well-formed
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format";
-            }
+// session_start(); 
+// if(isset($_SESSION['id'])){
+//     echo "<script type='text/javascript'>window.history.go(-1)</script>";
+// }
+$passwordErr = $emailErr = $password = $email = $dbErr = "";
+error_reporting(E_ERROR | E_PARSE);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "<script type='text/javascript'> showLogin()</script>";
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
         }
-        if (empty($_POST["password"])) {
-            $passwordErr = "password is required";
-        } else {
-            $password = test_input($_POST['password']);
-        }
-        if ($emailErr == "" && $passwordErr == "") {
-            include("../assets/php/dbcon.php");
-            $sql = "SELECT id,is_tutor FROM users WHERE email = '$email' AND password = '$password'";
-            // $sql = "SELECT * FROM users";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-                $_SESSION["id"] = $row["id"];
-                $_SESSION["is_tutor"] = $row["is_tutor"];
-                echo "<script type='text/javascript'>location.href = '../index.php';</script>";
-            } else {
-                $dbErr = "email or password is incorrect";
-            }
-        }
-        $_SESSION['LAST_ACTIVITY'] = time();
     }
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+    if (empty($_POST["password"])) {
+        $passwordErr = "password is required";
+    } else {
+        $password = test_input($_POST['password']);
     }
+    if ($emailErr == "" && $passwordErr == "") {
+        include("../assets/php/dbcon.php");
+        $sql = "SELECT id,is_tutor FROM users WHERE email = '$email' AND password = '$password'";
+        // $sql = "SELECT * FROM users";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $_SESSION["id"] = $row["id"];
+            $_SESSION["is_tutor"] = $row["is_tutor"];
+            echo "<script type='text/javascript'>location.href = '../index.php';</script>";
+        } else {
+            $dbErr = "email or password is incorrect";
+        }
+    }
+    $_SESSION['LAST_ACTIVITY'] = time();
+}
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,11 +53,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body class="flex-container-columns">
-    <?php include '../assets/php/header.php'; ?>
+    <?php
+    include '../assets/php/header.php';
+    ?>
     <div class="flex-container-column flex-center margin-top-82 margin-bottom-82">
         <img src="../assets/png/login_pelitaku.png" alt="">
         <div class="flex-container-row column-gap-40 margin-top-82">
@@ -77,37 +79,14 @@
             <?php echo $dbErr ?>
             <div class="flex-container-row flex-between width-full">
                 <input class="button continue" type="submit" value="Masuk">
-                <a href="./tutee/signup_tutee_1.php"><input class="button abort" type="button" value="Daftar">  </input></a>
+                <a href="./tutee/signup_tutee_1.php"><input class="button abort" type="button" value="Daftar"> </input></a>
             </div>
         </form>
     </div>
 
-    <footer class="flex-container-column flex-center footer">
-        <div class="flex-container-row flex-between footer-menu">
-            <ul class="footer-menu__left">
-                <li><b>Tentang</b></li>
-                <li>Siapa kami? </li>
-                <li>Syarat dan Ketentuan</li>
-                <li>Kebijakan Privasi</li>
-                <li>FAQ</li>
-            </ul>
-            <ul class="footer-menu__center">
-                <li><b>Kelas</b></li>
-                <li>Mulai Kelas</li>
-                <li>Lihat subjek kami</li>
-            </ul>
-            <ul class="footer-menu__right">
-                <li><b>Menjadi tutor</b></li>
-                <li>Keuntungan menjadi tutor</li>
-                <li>Daftar menjadi tutor</li>
-                <li>Masuk</li>
-            </ul>
-        </div>
-        <div class="width-1280">
-            <hr class="line">
-            <p align="center">© Pelitaku, 2021</p>
-        </div>
-    </footer>
+    <?php
+    include '../assets/php/footer.php'
+    ?>
     <script src="../scripts/login.js"></script>
 </body>
 
