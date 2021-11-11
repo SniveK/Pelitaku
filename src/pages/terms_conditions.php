51ad3695cd6938b891635078e3e06402e1f369c1
marcellinus-witarsah <aw.marcel@gmail.com>
2021-11-11 11:58:00 +0700

    modifikasi hasil kerja DP
    ubah faq, privacy policy, terms and conditionm, siapa kami jadi php

---
+++ src/pages/terms_conditions.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- <link rel="stylesheet" type="text/css" href="../css/aboutpage.css" /> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/jpg" href="../assets/png/icon_pelitaku.png" />
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Pelitaku - Syarat dan Ketentuan</title>
+</head>
+
+<body>
+    <?php
+    include '../assets/php/header.php'
+    ?>
+
+
+
+
+    <div class="flex-container-column flex-center ">
+        <div class="flex-container-row margin-top-82 ">
+            <div class="title">
+                <h1>Syarat dan Ketentuan</h1>
+            </div>
+        </div>
+
+        <div class="flex-container-row width-1280">
+            <hr class="line">
+        </div>
+
+        <div class="flex-container-column width-1280 margin-bottom-82 line-height-30 row-gap-20 font-size-18 text-align-justify">
+            <div>
+                Selamat datang di Pelitaku!
+            </div>
+
+            <div>
+                Syarat dan ketentuan ini menguraikan aturan dan peraturan untuk penggunaan situs Web Pelitaku.
+            </div>
+
+            <div>
+                Dengan mengakses situs web ini, kami menganggap Anda menerima syarat dan ketentuan ini. Jangan terus menggunakan situs ini jika anda tidak menyetujui seluruh Syarat dan Ketentuan yang tercantum di halaman ini.
+            </div>
+
+            <div>
+                Terminologi berikut berlaku untuk Syarat dan Ketentuan ini, Pernyataan Privasi dan Pemberitahuan Penafian dan semua Perjanjian: "Klien", "Anda" dan "Anda" mengacu pada Anda, orang yang masuk ke situs web ini dan mematuhi syarat dan ketentuan Pelitaku. "Perusahaan", "Kami Sendiri", "Kami", "Kami" dan "Kami", mengacu pada Pelitaku. "Pihak", "Para Pihak", atau "Kami", mengacu pada Klien dan kami sendiri. Semua istilah mengacu pada penawaran, penerimaan, dan pertimbangan pembayaran yang diperlukan untuk melakukan proses bantuan kami kepada Klien dengan cara yang paling tepat untuk tujuan yang jelas dalam memenuhi kebutuhan Klien sehubungan dengan penyediaan layanan yang dinyatakan Pelitaku, sesuai dengan dan tunduk pada, hukum yang berlaku di Indonesia. Setiap penggunaan terminologi di atas atau kata-kata lain dalam bentuk tunggal, jamak, huruf besar dan/atau dia, dianggap dapat dipertukarkan dan oleh karena itu mengacu pada yang sama.
+            </div>
+
+            <b class="font-size-20">
+                Cookie
+            </b>
+            <div>
+                Kami menerapkan penggunaan cookie. Dengan mengakses pelitaku, Anda setuju untuk menggunakan cookie sesuai dengan Kebijakan Privasi pelitaku. Sebagian besar situs web interaktif menggunakan cookie untuk memungkinkan kami mengambil detail pengguna untuk setiap kunjungan. Cookie digunakan oleh situs web kami untuk mengaktifkan fungsionalitas area tertentu untuk memudahkan orang mengunjungi situs web kami. Beberapa mitra afiliasi/iklan kami juga dapat menggunakan cookie.
+            </div>
+
+            <b class="font-size-20">
+                Lisensi
+            </b>
+            <div>
+                Kecuali dinyatakan lain, pelitaku dan/atau pemberi lisensinya memiliki hak kekayaan intelektual untuk semua materi di pelitaku. Semua hak kekayaan intelektual dilindungi. Anda dapat mengakses ini dari pelitaku untuk penggunaan pribadi Anda dengan batasan yang diatur dalam syarat dan ketentuan ini.
+            </div>
+            <b class="font-size-20">
+                Anda tidak bisa:
+            </b>
+            <div>
+                <ul>
+                    <li>Publikasikan ulang materi dari pelitaku.</li>
+                    <li>Menjual, menyewakan, atau mensublisensikan materi dari pelitaku.</li>
+                    <li>Mereproduksi, menggandakan, atau menyalin materi dari pelitaku.</li>
+                    <li> Distribusi ulang konten dari pelitaku.</li>
+                    <li>Perjanjian ini akan dimulai pada tanggal Perjanjian ini.</li>
+                </ul>
+            </div>
+            <div>
+                Bagian dari situs web ini menawarkan kesempatan bagi pengguna untuk memposting dan bertukar pendapat dan informasi di area tertentu dari situs web. pelitaku tidak memfilter, mengedit, menerbitkan, atau meninjau Komentar sebelum kehadirannya di situs web. Komentar tidak mencerminkan pandangan dan pendapat pelitaku, agen dan/atau afiliasinya. Komentar mencerminkan pandangan dan pendapat orang yang memposting pandangan dan pendapat mereka. Sejauh diizinkan oleh undang-undang yang berlaku, pelitaku tidak akan bertanggung jawab atas Komentar atau atas segala kewajiban, kerusakan atau pengeluaran yang disebabkan dan/atau diderita sebagai akibat dari penggunaan dan/atau posting dan/atau tampilan Komentar pada ini.
+            </div>
+
+            <div>
+                Pelitaku berhak untuk memantau semua Komentar dan menghapus Komentar apa pun yang dapat dianggap tidak pantas, menyinggung, atau menyebabkan pelanggaran terhadap Syarat dan Ketentuan ini.
+            </div>
+
+            <b class="font-size-20">
+                Anda menjamin dan menyatakan bahwa:
+            </b>
+
+            <div>
+                Anda berhak memposting Komentar di situs web kami dan memiliki semua lisensi dan persetujuan yang diperlukan untuk melakukannya.
+                Komentar tidak menyerang hak kekayaan intelektual apa pun, termasuk namun tidak terbatas pada hak cipta, paten, atau merek dagang dari pihak ketiga mana pun.
+                Komentar tidak mengandung materi yang memfitnah, memfitnah, menyinggung, tidak senonoh atau melanggar hukum yang merupakan pelanggaran privasi.
+                Komentar tidak akan digunakan untuk meminta atau mempromosikan bisnis atau kebiasaan atau menyajikan aktivitas komersial atau aktivitas yang melanggar hukum.
+                Anda dengan ini memberikan pelitaku lisensi non-eksklusif untuk menggunakan, mereproduksi, mengedit, dan mengizinkan orang lain untuk menggunakan, mereproduksi, dan mengedit Komentar Anda dalam segala bentuk, format, atau media.
+            </div>
+
+        </div>
+    </div>
+
+    <?php
+    include '../assets/php/footer.php'
+    ?>
+</body>
+
+</html>
