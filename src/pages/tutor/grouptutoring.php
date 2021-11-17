62495a50a0be741c3c05a97ac29ac7a91790f0a5
marcellinus-witarsah <aw.marcel@gmail.com>
2021-11-17 16:27:19 +0700

    ganti nama file doang - Marcel

--- src/pages/tutor/grouptutoring.php
+++ src/pages/tutor/grouptutoring.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 
-<html lang="id" >
+<html lang="id">
 
-    <head>
-        <meta charset="UTF-8"> 
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../../css/grouptutoring.css">
-		
-		<title> Pelitaku </title>
-		<link rel="icon" href="logo.png">
-		
-    </head>
-	
-    <body class="background">
-		<img src="../../assets/png/icon_message.png" alt="" style="position: fixed; right: 25px; top: 50%;" ">
-        <div class="header">
-			<object data="../../assets/svg/icon_pelitaku.svg" type="" width="28px" "></object>
-			<p class="push">pembelajaran</p>
-			<p>inbox</p>
-			<p>profil</p>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../../css/grouptutoring.css">
+
+	<title> Pelitaku </title>
+	<link rel="icon" href="logo.png">
+
+</head>
+
+<body class="background">
+	<img src="../../assets/png/icon_message.png" alt="" style="position: fixed; right: 25px; top: 50%;" ">
+        <div class=" header">
+	<object data="../../assets/svg/icon_pelitaku.svg" type="" width="28px" "></object>
+			<p class=" push">pembelajaran</p>
+		<p>inbox</p>
+		<p>profil</p>
 		</div>
-		
+
 		<div class="maincontent" style="margin: 3% 10% 5% 10%">
 			<h2> Group Tutoring </h2>
 			<hr>
@@ -28,21 +28,21 @@
 			<img src="../../assets/png/banner3.png" alt="" width="100%">
 			<div style="height: 20px;"></div>
 			<b> Studio Desain Dasar </b>
-			<p> Nirmana Kayu </p> 
+			<p> Nirmana Kayu </p>
 			<div style="height: 10px;"></div>
 			<div style="display: flex;">
-				
+
 				<div style=" width: 200px; height:270px; position: relative; margin-right: 25px; padding: 0%;">
-                    <img src="../../assets/png/contoh_tutor.png" alt="" width="200px" height="270px" >
-                    <div style="position: absolute; bottom: 0px; left: 0px; "><img src="../../assets/png/white_overlay.png" alt=""></div>
-                    <div style="position: absolute;bottom: 25px;right: 16px; color: black;font-weight: 600;">Claresta Y. Halim</div>
-                    <div style="position: absolute;bottom: 8px;right: 16px; color: black;">Desain Produk, 2019</div>
-                    <div style="position: absolute;bottom: 35px; left: 110px; "><img src="../../assets/png/starbar_grey.png" alt=""></div>
-                    <div style="position: absolute;bottom: 35px; left: 110px;  width: 81px; overflow: hidden;">
-                        <img src="../../assets/png/starbar_yellow.png" alt="" style="width: 81px; margin-left: 0px;">
-                    </div>
-                </div>
-				
+					<img src="../../assets/png/contoh_tutor.png" alt="" width="200px" height="270px">
+					<div style="position: absolute; bottom: 0px; left: 0px; "><img src="../../assets/png/white_overlay.png" alt=""></div>
+					<div style="position: absolute;bottom: 25px;right: 16px; color: black;font-weight: 600;">Claresta Y. Halim</div>
+					<div style="position: absolute;bottom: 8px;right: 16px; color: black;">Desain Produk, 2019</div>
+					<div style="position: absolute;bottom: 35px; left: 110px; "><img src="../../assets/png/starbar_grey.png" alt=""></div>
+					<div style="position: absolute;bottom: 35px; left: 110px;  width: 81px; overflow: hidden;">
+						<img src="../../assets/png/starbar_yellow.png" alt="" style="width: 81px; margin-left: 0px;">
+					</div>
+				</div>
+
 				<div>
 					<p> Kuota jumlah peserta: 3 orang </p>
 					<p> Rabu, 20 Oktober 2021 </p>
@@ -74,10 +74,10 @@
 				<button class="primarywhite"> Keluar</button>
 			</div>
 		</div>
-		
-        <div class="footer">
+
+		<div class="footer">
 			<div class="footermenu">
-				<div> 
+				<div>
 					<b>Tentang</b>
 					<p>Siapa kami? </p>
 					<p>Syarat dan Ketentuan</p>
@@ -88,17 +88,17 @@
 					<p>Mulai Kelas</p>
 					<p>Lihat subjek kami</p>
 				</div>
-				<div align="right"> 
+				<div align="right">
 					<b>Menjadi tutor</b>
 					<p>Keuntungan menjadi tutor</p>
 					<p>Daftar menjadi tutor</p>
 					<p>Masuk</p>
 				</div>
 			</div>
-            <hr>
-            <p align="center">© Pelitaku, 2021</p>
-        </div>
-		
-    </body>
+			<hr>
+			<p align="center">© Pelitaku, 2021</p>
+		</div>
+
+</body>
 
 </html>
