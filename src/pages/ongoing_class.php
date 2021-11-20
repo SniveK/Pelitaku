ffcd3ebb2f75f8298480699271c6f1b501c48fa7
Michelle Callista <michellecalista94@gmail.com>
2021-11-20 21:19:40 +0700

    Benerin yang lama

--- src/pages/ongoing_class.php
+++ src/pages/ongoing_class.php
@@ -5,8 +5,8 @@
     <head>
         <meta charset="UTF-8"> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../../css/ongoingclass.css">
-		<link rel="stylesheet" href="../../style.css">
+        <link rel="stylesheet" href="../css/ongoingclass.css">
+		<link rel="stylesheet" href="../style.css">
 		
 		
 		<title> Pelitaku </title>
@@ -15,21 +15,20 @@
     </head>
 	
     <body class="background">
-		<img src="../../assets/png/icon_message.png" alt="" style="position: fixed; right: 25px; top: 50%;">
 		
-		<?php include './assets/php/header.php'; ?>
+		<?php include '../assets/php/header.php'; ?>
 		
-		<div class="maincontent" style="margin: 22px 10% 36px 10%;">
-			<p class="judul"> Kelas Berlangsung </p>
+		<div class="maincontent">
+			<p class="title"> Kelas Berlangsung </p>
 			<hr>
 			<div>
 				<div class="ongoing"> 
 					<div style="width: 64px;"> 
-						<p style="font-size:36px; line-height:44px; margin:0; text-align:center;"> 15 Okt </p> 
+						<p class="ongoingdate"> 29 Nov </p> 
 					</div>
-					<div style="width:728px; padding:0 67px 0 67px; box-sizing: border-box;">
-						<p style="font-size:48px; font-weight:300; line-height:58px;"> Gambar 2</>
-						<p style="font-size:22px; font-weight:600; line-height:27px;"> Anda belum menyelesaikan 1 tugas </p>
+					<div style="width:728px; padding:0 67px 0 67px;">
+						<p class="ongoingheading"> Gambar 2</>
+						<p class="ongoingsubheading"> Anda belum menyelesaikan 1 tugas </p>
 					</div>
 					<div style="display: flex; align-items: center;">
 						<button class="primary"> Selesaikan </button>
@@ -37,11 +36,11 @@
 				</div>
 				<div class="ongoing"> 
 					<div style="width: 64px;"> 
-						<p style="font-size:36px; line-height:44px; margin:0; text-align:center;"> 15 Okt </p> 
+						<p class="ongoingdate"> 15 Okt </p> 
 					</div>
-					<div style="width:728px; padding:0 67px 0 67px; box-sizing: border-box;">
-						<p style="font-size:48px; font-weight:300; line-height:58px;"> Gambar 2</>
-						<p style="font-size:22px; font-weight:600; line-height:27px;"> Anda belum menyelesaikan 1 tugas </p>
+					<div style="width:728px; padding:0 67px 0 67px;">
+						<p class="ongoingheading"> Gambar 2</>
+						<p class="ongoingsubheading"> Anda belum menyelesaikan 1 tugas </p>
 					</div>
 					<div style="display: flex; align-items: center;">
 						<button class="primary"> Selesaikan </button>
@@ -49,39 +48,47 @@
 				</div>
 			</div>
 			
-			<p style="font-size:22px; font-weight:bold; line-height:24px; margin-top: 56px;"> Riwayat Kelas </p>
+			<p class="title" style="margin-top: 56px;"> Riwayat Kelas </p>
 			<hr>
 			<div>
 				<div class="history">
 					<p style="margin-right: 107px;"> 15/09 </p> 
 					<p> Studio Desain Dasar </p>
-					<a href="" class="push"><img src="../../assets/png/icon_right.png" alt="" ></a>
+					<a href="" class="push"><img src="../assets/png/icon_right.png" alt="" ></a>
 				</div>
 				<div class="history">
 					<p style="margin-right: 107px;"> 08/09 </p> 
 					<p> Sejarah </p>
-					<a href="" class="push"><img src="../../assets/png/icon_right.png" alt="" ></a>
+					<a href="" class="push"><img src="../assets/png/icon_right.png" alt="" ></a>
 				</div>
 				<div class="history">
 					<p style="margin-right: 107px;"> 25/08 </p> 
 					<p> Gambar 2 </p>
-					<a href="" class="push"><img src="../../assets/png/icon_right.png" alt="" ></a>
+					<a href="" class="push"><img src="../assets/png/icon_right.png" alt="" ></a>
 				</div>
 				<div class="history">
 					<p style="margin-right: 107px;"> 18/08 </p> 
 					<p> Gambar 2 </p>
-					<a href="" class="push"><img src="../../assets/png/icon_right.png" alt="" ></a>
+					<a href="" class="push"><img src="../assets/png/icon_right.png" alt="" ></a>
 				</div>
 				<div class="history">
 					<p style="margin-right: 107px;"> 12/08 </p> 
 					<p> Studio Desain Dasar </p>
-					<a href="" class="push"><img src="../../assets/png/icon_right.png" alt="" ></a>
+					<a href="" class="push"><img src="../assets/png/icon_right.png" alt="" ></a>
 				</div>
+			</div>
+			
+			<div class="search-nav" align="right">
+				<a href=""><img src="../assets/png/icon_left.png" alt=""></a>
+				<a href="">1</a>
+				<a href="">.</a>
+				<a href="">.</a>
+				<a href=""><img src="../assets/png/icon_right.png" alt=""></a>
 			</div>
 			
 		</div>
 		
-		<?php include './assets/php/footer.php'; ?>
+		<?php include "../assets/php/footer.php"; ?>
 		
     </body>
 
--- src/pages/tutor/grouptutoring.php
+++ src/pages/tutor/grouptutoring.php
@@ -6,6 +6,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="../../css/grouptutoring.css">
+	<link rel="stylesheet" href="../../style.css">
 
 	<title> Pelitaku </title>
 	<link rel="icon" href="logo.png">
@@ -13,15 +14,10 @@
 </head>
 
 <body class="background">
-	<img src="../../assets/png/icon_message.png" alt="" style="position: fixed; right: 25px; top: 50%;" ">
-        <div class=" header">
-	<object data="../../assets/svg/icon_pelitaku.svg" type="" width="28px" "></object>
-			<p class=" push">pembelajaran</p>
-		<p>inbox</p>
-		<p>profil</p>
-		</div>
-
-		<div class="maincontent" style="margin: 3% 10% 5% 10%">
+        
+		<?php include './assets/php/header.php';?>
+		
+		<div class="maincontent" style="padding: 22px 10% 36px 10%">
 			<h2> Group Tutoring </h2>
 			<hr>
 			<div style="height: 20px;"></div>
@@ -31,18 +27,16 @@
 			<p> Nirmana Kayu </p>
 			<div style="height: 10px;"></div>
 			<div style="display: flex;">
-
-				<div style=" width: 200px; height:270px; position: relative; margin-right: 25px; padding: 0%;">
-					<img src="../../assets/png/contoh_tutor.png" alt="" width="200px" height="270px">
-					<div style="position: absolute; bottom: 0px; left: 0px; "><img src="../../assets/png/white_overlay.png" alt=""></div>
-					<div style="position: absolute;bottom: 25px;right: 16px; color: black;font-weight: 600;">Claresta Y. Halim</div>
-					<div style="position: absolute;bottom: 8px;right: 16px; color: black;">Desain Produk, 2019</div>
-					<div style="position: absolute;bottom: 35px; left: 110px; "><img src="../../assets/png/starbar_grey.png" alt=""></div>
-					<div style="position: absolute;bottom: 35px; left: 110px;  width: 81px; overflow: hidden;">
-						<img src="../../assets/png/starbar_yellow.png" alt="" style="width: 81px; margin-left: 0px;">
-					</div>
-				</div>
-
+				
+				<div class="tutorpilihan" style="margin-right: 20px"> <div>
+					<img src="../../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
+					<div class="tutorpic"> <img src="../../assets/png/white_overlay.png" alt=""> </div>
+					<div class="tutorname"> Claresta Y. Halim </div>
+					<div class="tutormajor"> Desain Produk, 2019 </div>
+					<div class="starratinggrey"> <img src="../../assets/png/starbar_grey.png" alt="grey stars"> </div>
+					<div class="starratingyellow"> <img src="../../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;"> </div>
+				</div> </div>
+				
 				<div>
 					<p> Kuota jumlah peserta: 3 orang </p>
 					<p> Rabu, 20 Oktober 2021 </p>
@@ -54,15 +48,19 @@
 			<div class="peserta">
 				<p> Peserta yang sudah mendaftar: </p>
 				<div class="search-result">
-					<div style="display: flex; align-content: center;">
-						<div style="margin: auto 10px auto 0;"> <img src="../../assets/png/avatar.jpg" alt="" class="avatar" width="60px"> </div>
+					<div>
+						<div style="margin: auto 10px auto 0;">
+							<img src="../../assets/png/avatar.jpg" alt="" class="avatar" width="60px">
+						</div>
 						<div>
 							<p>Desain Produk, 2019</p>
 							<b>Karen Hartanto</b>
 						</div>
 					</div>
-					<div style="display: flex; align-content: center;">
-						<div style="margin: auto 10px auto 0;"> <img src="../../assets/png/avatar.jpg" alt="" class="avatar" width="60px"> </div>
+					<div>
+						<div style="margin: auto 10px auto 0;">
+							<img src="../../assets/png/avatar.jpg" alt="" class="avatar" width="60px">
+						</div>
 						<div>
 							<p>Desain Produk, 2019</p>
 							<b>Karen Hartanto</b>
@@ -74,30 +72,8 @@
 				<button class="primarywhite"> Keluar</button>
 			</div>
 		</div>
-
-		<div class="footer">
-			<div class="footermenu">
-				<div>
-					<b>Tentang</b>
-					<p>Siapa kami? </p>
-					<p>Syarat dan Ketentuan</p>
-					<p>Kebijakan Privasi</p>
-				</div>
-				<div>
-					<b>Kelas</b>
-					<p>Mulai Kelas</p>
-					<p>Lihat subjek kami</p>
-				</div>
-				<div align="right">
-					<b>Menjadi tutor</b>
-					<p>Keuntungan menjadi tutor</p>
-					<p>Daftar menjadi tutor</p>
-					<p>Masuk</p>
-				</div>
-			</div>
-			<hr>
-			<p align="center">© Pelitaku, 2021</p>
-		</div>
+		
+		<?php include './assets/php/footer.php'; ?>
 
 </body>
 
