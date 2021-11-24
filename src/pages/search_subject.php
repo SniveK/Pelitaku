c41833840f2a4c81fd715345615fe8f0e342aaaa
MC <61784086+MCallista@users.noreply.github.com>
2021-11-24 16:09:10 +0700

    Search update

--- src/pages/search_subject.php
+++ src/pages/search_subject.php
@@ -11,70 +11,47 @@
 
 </head>
 
-<body class="background">
-    <img src="../assets/png/icon_message.png" alt="" class="message-icon">
-    <div class="header">
-        <object data="../../assets/svg/icon_pelitaku.svg" type="" class="header-icon"></object>
-        <p class="push">pembelajaran</p>
-        <p>inbox</p>
-        <p>profil</p>
-    </div>
+<body>
+    
+	<?php include '../assets/php/header.php'; ?>
 
-    <div class="maincontent">
-        <?php include "../assets/php/searchbar.php" ?>
-        <div style="height: 20px;"></div>
+	<div class="background">
+		<div class="maincontent">
+			
+			<?php include './assets/php/searchbar.php';?>
+			
+			<div style="height: 20px;"></div>
 
-        <div class="search-result">
-            <?php
-            include '../assets/php/dbcon.php';
-            if ($_SERVER["REQUEST_METHOD"] == "GET") {
-                $sql = "SELECT subject,topic FROM class WHERE subject LIKE '%" . $_GET['subject'] . "%' OR topic LIKE '%" . $_GET['subject'] . "%'";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<div><b>" . $row['subject'] . "</b><p>" . $row['topic'] . "</p></div>";
-                    }
-                } else {
-                }
-            }
-            ?>
+			<div class="search-result">
+				<?php
+				include '../assets/php/dbcon.php';
+				if ($_SERVER["REQUEST_METHOD"] == "GET") {
+					$sql = "SELECT subject,topic FROM class WHERE subject LIKE '%" . $_GET['subject'] . "%' OR topic LIKE '%" . $_GET['subject'] . "%'";
+					$result = $conn->query($sql);
+					if ($result->num_rows > 0) {
+						while ($row = $result->fetch_assoc()) {
+							echo "<div><b>" . $row['subject'] . "</b><p>" . $row['topic'] . "</p></div>";
+						}
+					} else {
+					}
+				}
+				?>
+			</div>
 
-        </div>
+			<div class="search-nav" align="right">
+				<a href=""><img src="../assets/png/icon_left.png" alt=""></a>
+				<a href="">1</a>
+				<a href="">.</a>
+				<a href="">.</a>
+				<a href=""><img src="../assets/png/icon_right.png" alt=""></a>
+			</div>
 
-        <div class="search-nav" align="right">
-            <a href=""><img src="../assets/png/icon_left.png" alt=""></a>
-            <a href="">1</a>
-            <a href="">.</a>
-            <a href="">.</a>
-            <a href=""><img src="../assets/png/icon_right.png" alt=""></a>
-        </div>
+			<div style="height: 20px;"></div>
+			
+		</div>
+	</div>
 
-        <div style="height: 20px;"></div>
-    </div>
-
-    <footer class="footer">
-        <div class="footermenu">
-            <div>
-                <b>Tentang</b>
-                <p>Siapa kami? </p>
-                <p>Syarat dan Ketentuan</p>
-                <p>Kebijakan Privasi</p>
-            </div>
-            <div>
-                <b>Kelas</b>
-                <p>Mulai Kelas</p>
-                <p>Lihat subjek kami</p>
-            </div>
-            <div align="right">
-                <b>Menjadi tutor</b>
-                <p>Keuntungan menjadi tutor</p>
-                <p>Daftar menjadi tutor</p>
-                <p>Masuk</p>
-            </div>
-        </div>
-        <hr width="95%" align="center">
-        <p align="center">© Pelitaku, 2021</p>
-    </footer>
+    <?php include './assets/php/footer.php'; ?>
 
     <script src="script.js" type="application/javascript"></script>
 </body>
--- src/pages/search_tutor.php
+++ src/pages/search_tutor.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="../css/search.css">
     <link rel="stylesheet" href="../css/profile.css">
     <title> Pelitaku </title>
@@ -13,35 +13,15 @@
 
 </head>
 
-<body class="background">
-    <img src="../assets/png/icon_message.png" alt="" class="message-icon">
-    <!-- <div class="header">
-        <object data="../assets/svg/icon_pelitaku.svg" type="" class="header-icon"></object>
-        <p class="push">pembelajaran</p>
-        <p>inbox</p>
-        <p>profil</p>
-    </div> -->
-    <?php include "../assets/php/header.php"; ?>
+<body>
 
-    <div class="flex-container-column flex-center margin-top-82 margin-bottom-82 row-gap-40">
-        <div class="flex-container-row flex-center column-gap-20">
-            <object data="../assets/svg/logo_pelitaku.svg" type="" width="204px" height="67.14px"></object>
-            <div class="position-relative">
-                <form action="">
-                    <input type="text" placeholder="Cari lokasi" class="input-location">
-                    <input src="../assets/png/icon_gps.png" alt="Icon magnifying glass" class="search-icon" type="image" style="border: none">
-                </form>
-            </div>
-            <div class="position-relative">
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
-                    <input type="text" placeholder="Cari subjek...." class="input-subject" name="subject">
-                    <input src="../assets/png/icon_search.png" alt="Icon magnifying glass" class="search-icon" type="image" style="border: none">
-                </form>
-            </div>
-        </div>
-        <div class="flex-container-column row-gap-40 width-1280">
-
-            <div class="flex-container-row flex-center column-gap-20">
+    <?php include '../assets/php/header.php'; ?>
+	
+	<div class="background">
+		<div class="maincontent">
+			<?php include './assets/php/searchbar.php';?>
+			
+			<div class="searchtitle">
                 <h2>
                     Tutor Desain Produk 1 yang tersedia berdasarkan
                 </h2>
@@ -51,165 +31,78 @@
                     <option value="tingkat">Tingkat</option>
                 </select>
             </div>
-
-            <div class="flex-container-column column-gap-20 tutor-border">
-                <div class="margin-left-90">
-                    <h4>Special Pick</h4>
+			
+			<div class="specialpicks">
+                <div>
+                    <h3>Special Pick</h3>
                 </div>
-                <div class="flex-container-row flex-center tutor-images">
-                    <div style=" width: 200px; height:270px; position: relative; margin: 0; padding: 0%;">
-                        <img src="../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
-                        <div style="position: absolute; bottom: 0px; left: 0px;"><img src="../assets/png/white_overlay.png" alt=""></div>
-                        <div style="position: absolute;bottom: 25px;right: 16px; color: black;font-weight: 600;">Claresta Y. Halim</div>
-                        <div style="position: absolute;bottom: 8px;right: 16px; color: black;">Desain Produk, 2019</div>
-                        <div style="position: absolute;bottom: 35px; left: 110px; "><img src="../assets/png/starbar_grey.png" alt="grey stars"></div>
-                        <div style="position: absolute;bottom: 35px; left: 110px;  width: 81px; overflow: hidden;">
-                            <img src="../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;">
-                        </div>
-                    </div>
-                    <div style=" width: 200px; height:270px; position: relative; margin: 0; padding: 0%;">
-                        <img src="../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
-                        <div style="position: absolute; bottom: 0px; left: 0px;"><img src="../assets/png/white_overlay.png" alt=""></div>
-                        <div style="position: absolute;bottom: 25px;right: 16px; color: black;font-weight: 600;">Claresta Y. Halim</div>
-                        <div style="position: absolute;bottom: 8px;right: 16px; color: black;">Desain Produk, 2019</div>
-                        <div style="position: absolute;bottom: 35px; left: 110px; "><img src="../assets/png/starbar_grey.png" alt="grey stars"></div>
-                        <div style="position: absolute;bottom: 35px; left: 110px;  width: 81px; overflow: hidden;">
-                            <img src="../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;">
-                        </div>
-                    </div>
-                    <div style=" width: 200px; height:270px; position: relative; margin: 0; padding: 0%;">
-                        <img src="../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
-                        <div style="position: absolute; bottom: 0px; left: 0px;"><img src="../assets/png/white_overlay.png" alt=""></div>
-                        <div style="position: absolute;bottom: 25px;right: 16px; color: black;font-weight: 600;">Claresta Y. Halim</div>
-                        <div style="position: absolute;bottom: 8px;right: 16px; color: black;">Desain Produk, 2019</div>
-                        <div style="position: absolute;bottom: 35px; left: 110px; "><img src="../assets/png/starbar_grey.png" alt="grey stars"></div>
-                        <div style="position: absolute;bottom: 35px; left: 110px;  width: 81px; overflow: hidden;">
-                            <img src="../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;">
-                        </div>
-                    </div>
-                    <div style=" width: 200px; height:270px; position: relative; margin: 0; padding: 0%;">
-                        <img src="../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
-                        <div style="position: absolute; bottom: 0px; left: 0px;"><img src="../assets/png/white_overlay.png" alt=""></div>
-                        <div style="position: absolute;bottom: 25px;right: 16px; color: black;font-weight: 600;">Claresta Y. Halim</div>
-                        <div style="position: absolute;bottom: 8px;right: 16px; color: black;">Desain Produk, 2019</div>
-                        <div style="position: absolute;bottom: 35px; left: 110px; "><img src="../assets/png/starbar_grey.png" alt="grey stars"></div>
-                        <div style="position: absolute;bottom: 35px; left: 110px;  width: 81px; overflow: hidden;">
-                            <img src="../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;">
-                        </div>
-                    </div>
+                <div class="tutorpilihan flex-between">
+					<div>
+						<img src="../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
+						<div class="tutorpic"> <img src="../assets/png/white_overlay.png" alt=""> </div>
+						<div class="tutorname"> Claresta Y. Halim </div>
+						<div class="tutormajor"> Desain Produk, 2019 </div>
+						<div class="starratinggrey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
+						<div class="starratingyellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;"> </div>
+					</div>
+					<div>
+						<img src="../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
+						<div class="tutorpic"> <img src="../assets/png/white_overlay.png" alt=""> </div>
+						<div class="tutorname"> Claresta Y. Halim </div>
+						<div class="tutormajor"> Desain Produk, 2019 </div>
+						<div class="starratinggrey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
+						<div class="starratingyellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;"> </div>
+					</div>
+                    <div>
+						<img src="../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
+						<div class="tutorpic"> <img src="../assets/png/white_overlay.png" alt=""> </div>
+						<div class="tutorname"> Claresta Y. Halim </div>
+						<div class="tutormajor"> Desain Produk, 2019 </div>
+						<div class="starratinggrey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
+						<div class="starratingyellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;"> </div>
+					</div>
+					<div>
+						<img src="../assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
+						<div class="tutorpic"> <img src="../assets/png/white_overlay.png" alt=""> </div>
+						<div class="tutorname"> Claresta Y. Halim </div>
+						<div class="tutormajor"> Desain Produk, 2019 </div>
+						<div class="starratinggrey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
+						<div class="starratingyellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;"> </div>
+					</div>
                 </div>
             </div>
-
-            <div class="tutor-boxes">
-                <div class=" flex-container-column tutor-small-box">
-                    <div class="flex-container-row flex-between position-relative">
-                        <div class="circle width-45 height-45"></div>
-                        <div class="position-relative">
+			
+			<div class="search-result">
+				<div>
+					<div class="flex-container-row" style="align-content:center;">
+						<div style="margin: auto 80px auto 0;"> 
+							<img src="../assets/png/avatar.jpg" alt="" class="avatar" width="45px">
+						</div>
+						<div class="position-relative">
                             <div class="star-grey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
                             <div class="star-yellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars"> </div>
                         </div>
-                    </div>
-                    <hr class="width-full">
-                    <div class="flex-container-column flex-start row-gap-10">
-                        <div>Desain Produk, 2018</div>
-                        <div>Calvin</div>
-                        <div>Cengkareng, Jakarta Barat</div>
-                    </div>
-                </div>
+					</div>
+					<hr width="100%" style="margin:5px 0 5px 0;">
+					<div>
+						<p>Desain Produk, 2018</p>
+						<b>Calvin</b>
+						<p>Cengkareng, Jakarta Barat</p>
+					</div>
+				</div>
 
-                <div class="flex-container-column tutor-small-box">
-                    <div class="flex-container-row flex-between position-relative">
-                        <div class="circle width-45 height-45"></div>
-                        <div class="position-relative">
-                            <div class="star-grey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
-                            <div class="star-yellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars"> </div>
-                        </div>
-                    </div>
-                    <hr class="width-full">
-                    <div class="flex-container-column flex-start row-gap-10">
-                        <div>Desain Produk, 2018</div>
-                        <div>Calvin</div>
-                        <div>Cengkareng, Jakarta Barat</div>
-                    </div>
-                </div>
-
-                <div class="flex-container-column tutor-small-box">
-                    <div class="flex-container-row flex-between position-relative">
-                        <div class="circle width-45 height-45"></div>
-                        <div class="position-relative">
-                            <div class="star-grey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
-                            <div class="star-yellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars"> </div>
-                        </div>
-                    </div>
-                    <hr class="width-full">
-                    <div class="flex-container-column flex-start row-gap-10">
-                        <div>Desain Produk, 2018</div>
-                        <div>Calvin</div>
-                        <div>Cengkareng, Jakarta Barat</div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="tutor-boxes">
-                <div class="flex-container-column tutor-small-box">
-                    <div class="flex-container-row flex-between position-relative">
-                        <div class="circle width-45 height-45"></div>
-                        <div class="position-relative">
-                            <div class="star-grey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
-                            <div class="star-yellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars"> </div>
-                        </div>
-                    </div>
-                    <hr class="width-full">
-                    <div class="flex-container-column flex-start row-gap-10">
-                        <div>Desain Produk, 2018</div>
-                        <div>Calvin</div>
-                        <div>Cengkareng, Jakarta Barat</div>
-                    </div>
-                </div>
-                <div class="flex-container-column tutor-small-box">
-                    <div class="flex-container-row flex-between position-relative">
-                        <div class="circle width-45 height-45"></div>
-                        <div class="position-relative">
-                            <div class="star-grey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
-                            <div class="star-yellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars"> </div>
-                        </div>
-                    </div>
-                    <hr class="width-full">
-                    <div class="flex-container-column flex-start row-gap-10">
-                        <div>Desain Produk, 2018</div>
-                        <div>Calvin</div>
-                        <div>Cengkareng, Jakarta Barat</div>
-                    </div>
-                </div>
-                <div class="flex-container-column tutor-small-box">
-                    <div class="flex-container-row flex-between position-relative">
-                        <div class="circle width-45 height-45"></div>
-                        <div class="position-relative">
-                            <div class="star-grey"> <img src="../assets/png/starbar_grey.png" alt="grey stars"> </div>
-                            <div class="star-yellow"> <img src="../assets/png/starbar_yellow.png" alt="yellow stars"> </div>
-                        </div>
-                    </div>
-                    <hr class="width-full">
-                    <div class="flex-container-column flex-start row-gap-10">
-                        <div>Desain Produk, 2018</div>
-                        <div>Calvin</div>
-                        <div>Cengkareng, Jakarta Barat</div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="search-nav" align="right">
+			</div>
+			
+			<div class="search-nav" align="right">
                 <a href=""><img src="../assets/png/icon_left.png" alt=""></a>
                 <a href="">1</a>
                 <a href="">.</a>
                 <a href="">.</a>
                 <a href=""><img src="../assets/png/icon_right.png" alt=""></a>
             </div>
-        </div>
-    </div>
-
-
+			
+		</div>
+	</div>
 
     <?php include "../assets/php/footer.php"; ?>
 
