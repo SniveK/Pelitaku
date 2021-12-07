a96e78c29b3dac415ef487a25045da1fcfa91a72
marcellinus-witarsah <aw.marcel@gmail.com>
2021-12-07 17:59:20 +0700

    update index.php

--- src/index.php
+++ src/index.php
@@ -8,11 +8,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=1280, initial-scale=1">
+    <meta name="description" content="Pelitaku Homepage">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/style.css">
     <title> Pelitaku </title>
-    <link rel="icon" href="logo.png">
+    <link rel="icon" href="./assets/png/icon_pelitaku.png">
 
 </head>
 
@@ -25,80 +26,83 @@
             <form action="../../pages/tutee/search_subject.php" method="get">
                 <?php include './assets/php/searchbar.php'; ?>
             </form>
-            <img src="./assets/png/banner1.png" alt="Banner Yuk Belajar Bareng" width="100%">
+            <img src="./assets/png/banner1.png" alt="Banner Yuk Belajar Bareng" style="width: 1024px; height: 378px;">
 
             <div class="caption"> Kami memiliki puluhan tutor yang siap membimbingmu dalam belajar. </div>
             <div class="caption"> Belajar Desain dengan tutor pilihan kami </div>
 
-            <div class="tutorpilihan">
+            <div class="tutorpilihan column-gap-20">
                 <div>
-                    <img src="./assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
-                    <div class="tutorpic"> <img src="./assets/png/white_overlay.png" alt=""> </div>
+                    <img src="./assets/png/contoh_tutor.png" alt="Gambar Tutor" style="width: 208px; height: 272px;">
+                    <div class="tutorpic"> <img src="./assets/png/white_overlay.png" alt="white overlay" style="width: 200px; height: 60px;"> </div>
                     <div class="tutorname"> Claresta Y. Halim </div>
                     <div class="tutormajor"> Desain Produk, 2019 </div>
-                    <div class="starratinggrey"> <img src="./assets/png/starbar_grey.png" alt="grey stars"> </div>
-                    <div class="starratingyellow"> <img src="./assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;"> </div>
+                    <div class="starratinggrey"> <img src="./assets/png/starbar_grey.png" alt="grey stars" style="width: 81px; height: 22px; margin-left: 0px;"> </div>
+                    <div class="starratingyellow"> <img src="./assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; height: 22px; margin-left: 0px;"> </div>
                 </div>
 
                 <div>
-                    <img src="./assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
-                    <div class="tutorpic"> <img src="./assets/png/white_overlay.png" alt=""> </div>
+                    <img src="./assets/png/contoh_tutor.png" alt="Gambar Tutor" style="width: 208px; height: 272px;">
+                    <div class="tutorpic"> <img src="./assets/png/white_overlay.png" alt="white overlay" style="width: 200px; height: 60px;"> </div>
                     <div class="tutorname"> Claresta Y. Halim </div>
                     <div class="tutormajor"> Desain Produk, 2019 </div>
-                    <div class="starratinggrey"> <img src="./assets/png/starbar_grey.png" alt="grey stars"> </div>
-                    <div class="starratingyellow"> <img src="./assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;"> </div>
+                    <div class="starratinggrey"> <img src="./assets/png/starbar_grey.png" alt="grey stars" style="width: 81px; height: 22px; margin-left: 0px;"> </div>
+                    <div class="starratingyellow"> <img src="./assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; height: 22px; margin-left: 0px;"> </div>
                 </div>
 
                 <div>
-                    <img src="./assets/png/contoh_tutor.png" alt="Gambar Tutor" width="200px" height="270px">
-                    <div class="tutorpic"> <img src="./assets/png/white_overlay.png" alt=""> </div>
+                    <img src="./assets/png/contoh_tutor.png" alt="Gambar Tutor" style="width: 208px; height: 272px;">
+                    <div class="tutorpic"> <img src="./assets/png/white_overlay.png" alt="white overlay" style="width: 200px; height: 60px;"> </div>
                     <div class="tutorname"> Claresta Y. Halim </div>
                     <div class="tutormajor"> Desain Produk, 2019 </div>
-                    <div class="starratinggrey"> <img src="./assets/png/starbar_grey.png" alt="grey stars"> </div>
-                    <div class="starratingyellow"> <img src="./assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; margin-left: 0px;"> </div>
+                    <div class="starratinggrey"> <img src="./assets/png/starbar_grey.png" alt="grey stars" style="width: 81px; height: 22px; margin-left: 0px;"> </div>
+                    <div class="starratingyellow"> <img src="./assets/png/starbar_yellow.png" alt="yellow stars" style="width: 81px; height: 22px; margin-left: 0px;"> </div>
                 </div>
 
             </div>
 
-            <img src="./assets/png/banner2.png" alt="" width="100%">
+            <img src="./assets/png/banner2.png" alt="banner 2" style="width: 1024px; height: 378px;">
 
             <p class="caption"> Mata kuliah tren: </p>
 
-            <div>
-                <button class="primary1"> Desain </button>
-                <button class="primary1"> Gambar Teknik </button>
-                <button class="primary1"> Studio Desain </button>
-                <button class="primary1"> Ergonomi </button>
-                <button class="primary2"> Ergonomi </button>
-                </br>
-                <button class="primary1"> Sejarah </button>
-                <button class="primary1"> Presentasi Desain </button>
-                <button class="primary1"> Ekonomi </button>
-                <button class="primary1"> Ekonomi Mikro </button>
-                <button class="primary2"> Ekonomi Mikro </button>
+            <div class="flex-container-column row-gap-20">
+                <div class="flex-container-row column-gap-20">
+                    <button class="primary1"> Desain </button>
+                    <button class="primary1"> Gambar Teknik </button>
+                    <button class="primary1"> Studio Desain </button>
+                    <button class="primary1"> Ergonomi </button>
+                    <button class="primary1"> Ergonomi </button>
+                </div>
+                <div class="flex-container-row column-gap-20">
+                    <button class="primary1"> Sejarah </button>
+                    <button class="primary1"> Presentasi Desain </button>
+                    <button class="primary1"> Ekonomi </button>
+                    <button class="primary1"> Ekonomi Mikro </button>
+                    <button class="primary1"> Ekonomi Mikro </button>
+                </div>
             </div>
             <button class="secondary push"> lihat semua kelas </button>
 
             <div class="slideshow-container">
                 <div class="mySlides fade">
                     <div class="numbertext">1 / 5</div> <!-- Biar keliatan pindah -->
-                    <img src="./assets/png/banner3.png" style="width:100%">
+                    <img src="./assets/png/banner3.png" alt="banner 3" style="width: 1024px; height: 376px;">
                 </div>
                 <div class="mySlides fade">
                     <div class="numbertext">2 / 5</div>
-                    <img src="./assets/png/banner3.png" style="width:100%">
+                    <img src="./assets/png/banner3.png" alt="banner 3" style="width: 1024px; height: 376px;">
                 </div>
                 <div class="mySlides fade">
                     <div class="numbertext">3 / 5</div>
-                    <img src="./assets/png/banner3.png" style="width:100%">
+                    <img src="./assets/png/banner3.png" alt="banner 3" style="width: 1024px; height: 376px;">
                 </div>
                 <div class="mySlides fade">
                     <div class="numbertext">4 / 5</div>
-                    <img src="./assets/png/banner3.png" style="width:100%">
+                    <img src="./assets/png/banner3.png" alt="banner 3" style="width: 1024px; height: 376px;">
                 </div>
                 <div class="mySlides fade">
                     <div class="numbertext">5 / 5</div>
-                    <img src="./assets/png/banner3.png" style="width:100%">
+                    <img src="./assets/png/banner3.png" alt="banner 3" style="width: 1024px; height: 376px;">
                 </div>
                 <div class="dotgroup">
                     <span class="dot" onclick="currentSlide(1)"></span>
