e7ad46234ef848d4d8da0b1c3137c49aef4432d5
SniveK <46390179+SniveK@users.noreply.github.com>
2021-12-16 16:56:39 +0700

    final changes

--- src/pages/tutee/booking.php
+++ src/pages/tutee/booking.php
@@ -24,37 +24,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit-final"])) {
 </head>
 
 <body>
-    
-        <?php include '../../assets/php/header.php'; ?>
-        <div class="flex-container-row flex-center">
-            <div class="flex-container-column flex-center width-1280 margin-top-82 margin-bottom-82 row-gap-40">
-                Buat Sesi Belajar
-                <br>
-                <?php
-                echo $_POST["subject-tutor"];
-                ?>
-                <br>
-                Dengan Tutor:
-                <?php
-                echo $_POST["tutor-name"];
-                ?>
-                <form action="./calendar_booking.php" method="POST">
-                    <input class="background-transparent add-arrow-right width-650 text-align-left" type="submit" value="<?php echo (isset($_POST["time_start"])) ?  $_POST["fulldate"] . " " . $_POST["time_start"] . "-" . $_POST["time_end"]   : "Pilih Waktu?" ?>">
-                    <input type="text" name="subject-tutor" value="<?php echo $_POST["subject-tutor"] ?>" hidden>
-                    <input type="text" name="tutor-id" value="<?php echo $_POST["tutor-id"] ?>" hidden>
-                    <input type="text" name="tutor-name" value="<?php echo $_POST["tutor-name"] ?>" hidden>
-                </form>
-                <!-- <select class="add-arrow-down width-650" name="" id="">
+
+    <?php include '../../assets/php/header.php'; ?>
+    <div class="flex-container-row flex-center">
+        <div class="flex-container-column flex-center width-1280 margin-top-82 margin-bottom-82 row-gap-40">
+            <h1>Buat Sesi Belajar</h1>
+            <?php
+            echo $_POST["subject-tutor"];
+            ?>
+            <br>
+            Dengan Tutor:
+            <?php
+            echo $_POST["tutor-name"];
+            ?>
+            <form action="./calendar_booking.php" method="POST">
+                <input class="background-transparent add-arrow-right width-650 text-align-left" type="submit" value="<?php echo (isset($_POST["time_start"])) ?  $_POST["fulldate"] . " " . $_POST["time_start"] . "-" . $_POST["time_end"]   : "Pilih Waktu?" ?>">
+                <input type="text" name="subject-tutor" value="<?php echo $_POST["subject-tutor"] ?>" hidden>
+                <input type="text" name="tutor-id" value="<?php echo $_POST["tutor-id"] ?>" hidden>
+                <input type="text" name="tutor-name" value="<?php echo $_POST["tutor-name"] ?>" hidden>
+            </form>
+            <!-- <select class="add-arrow-down width-650" name="" id="">
                 <option disabled selected value> -- Pilih Mata Kuliah -- </option>
             </select> -->
-                <!-- PINDAH FORM KESINI -->
-                <form method="post" action="">
+            <!-- PINDAH FORM KESINI -->
+            <form method="post" action="">
                 <div class="flex-container-column flex-center margin-bottom-82 width-full row-gap-40">
 
                     <!-- <input type="text" name="date" value="<?php //echo $_POST["date"] 
                                                                 ?>" hidden> -->
 
-                    
+
                     <select class="add-arrow-down width-650" name="type" id="">
                         <option disabled selected value> -- Select Online/ Offline -- </option>
                         <option value="online"> Online </option>
@@ -65,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit-final"])) {
                 <!-- <a href="../../pages/tutee/calendar_booking.php?tutor-id=<?php //echo $_POST["tutor-id"] 
                                                                                 ?>&subject-tutor=<?php //echo $_POST["subject-tutor"] 
                                                                                                     ?>&tutor-name=<?php //echo $_POST["tutor-name"] 
-                                                                                                                ?>"> -->
+                                                                                                                    ?>"> -->
 
                 <!-- </a> -->
                 <textarea class="width-650 height-300" name="purpose" id="" placeholder="Ceritakan tujuan Anda di sesi ini"></textarea>
@@ -74,35 +73,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit-final"])) {
                     <a href="../../assets/php/logout.php"><input class='button abort' type='button' value='Batal' id='cancel'></a>
                 </div>
                 <div class="login-box flex-container-column hide flex-center width-650 login-border row-gap-20" id="confirmation">
-                <div class="flex-container-row flex-end width-full margin-top-15 margin-right-30">
-                    <img class="width-30" src="../../assets/png/icon_x.png" alt="" onclick="hidePopUp()">
-                </div>
-                <div class="flex-container-column text-align-center line-height-30 width-80-percent row-gap-20">
-                    <h1>Apakah anda yakin ingin book kelas ini?</h1>
-                    <div class="font-size-18">
-                        <span id="time-info">Jika anda booking kelas ini, anda tidak bisa booking
-                            kelas lain sampai anda menyelesaikan tugas kelas ini.</span>
+                    <div class="flex-container-row flex-end width-full margin-top-15 margin-right-30">
+                        <img class="width-30" src="../../assets/png/icon_x.png" alt="" onclick="hidePopUp()">
                     </div>
+                    <div class="flex-container-column text-align-center line-height-30 width-80-percent row-gap-20">
+                        <h1>Apakah anda yakin ingin book kelas ini?</h1>
+                        <div class="font-size-18">
+                            <span id="time-info">Jika anda booking kelas ini, anda tidak bisa booking
+                                kelas lain sampai anda menyelesaikan tugas kelas ini.</span>
+                        </div>
+                    </div>
+                    <!-- FORM YANG DIPINDAH -->
+                    <!-- <form class="flex-container-column flex-center margin-bottom-82 width-full row-gap-40" method="post" action=""> -->
+
+                    <input type="text" name="date" value="<?php echo $_POST["date"]  ?>" hidden>
+                    <input type="text" name="time_start" value="<?php echo $_POST["time_start"] ?>" hidden>
+                    <input type="text" name="time_end" value="<?php echo $_POST["time_end"] ?>" hidden>
+                    <input type="text" name="subject-tutor" value="<?php echo $_POST["subject-tutor"] ?>" hidden>
+                    <input type="text" name="tutor-id" value="<?php echo $_POST["tutor-id"] ?>" hidden>
+                    <input type="text" name="submit-final" value="FINAL" hidden>
+                    <input class="button continue" type="submit" value="FINAL">
+
                 </div>
-                <!-- FORM YANG DIPINDAH -->
-                <!-- <form class="flex-container-column flex-center margin-bottom-82 width-full row-gap-40" method="post" action=""> -->
-
-                <input type="text" name="date" value="<?php echo $_POST["date"]  ?>" hidden>
-                <input type="text" name="time_start" value="<?php echo $_POST["time_start"] ?>" hidden>
-                <input type="text" name="time_end" value="<?php echo $_POST["time_end"] ?>" hidden>
-                <input type="text" name="subject-tutor" value="<?php echo $_POST["subject-tutor"] ?>" hidden>
-                <input type="text" name="tutor-id" value="<?php echo $_POST["tutor-id"] ?>" hidden>
-                <input type="text" name="submit-final" value="FINAL" hidden>
-                <input class="button continue" type="submit" value="FINAL">
-
-            </div>
-                </form>
-            </div>
-            
+            </form>
         </div>
-        <?php include '../../assets/php/footer.php' ?>
-        <script src="../../scripts/booking.js"></script>
-   
+
+    </div>
+    <?php include '../../assets/php/footer.php' ?>
+    <script src="../../scripts/booking.js"></script>
+
 </body>
 
 </html>
